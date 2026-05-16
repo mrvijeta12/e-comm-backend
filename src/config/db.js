7c327ecba3da73a06471441d7eb7a941f3b80a8a
vijeta <vijetavarma1@gmail.com>
2026-05-16 21:38:47 +0530

    server file updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 
-// console.log("MONGO_URI:", process.env.MONGO_URL);
-// console.log("JWT_SECRET:", process.env.SECRET_KEY);
+console.log("MONGO_URL exists:", !!process.env.MONGO_URL);
+console.log("SECRET_KEY exists:", !!process.env.SECRET_KEY);
+
 const connection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
+
     console.log("Database Connected");
   } catch (error) {
     console.log("Error while connecting database:", error.message);
--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import connection from "./config/db.js";
 import app from "./index.js";
 
-const PORT = 5000;
+await connection();
 
-const startServer = async () => {
-  try {
-    await connection(); // wait for DB
-    app.listen(PORT, () => {
-      console.log("server is running on:", PORT);
-    });
-  } catch (error) {
-    console.log("Server failed to start");
-  }
-};
+export default app;
 
-startServer();
+//! for local host
+// const PORT = 5000;
+// const startServer = async () => {
+//   try {
+//     await connection(); // wait for DB
+//     app.listen(PORT, () => {
+//       console.log("server is running on:", PORT);
+//     });
+//   } catch (error) {
+//     console.log("Server failed to start");
+//   }
+// };
+
+// startServer();
